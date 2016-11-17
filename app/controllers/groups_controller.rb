47f84d2562dcf3fe3b51b6a453fd41fcdb5fb171
Stan <solve153@gmail.com>
2016-11-18 00:13:03 +0800

    add posts edit/destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,6 +16,7 @@ class GroupsController < ApplicationController
 	end
 
 	def edit
+		
 	end
 
 	def update
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,30 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def edit
+		@group = Group.find(params[:group_id])
+		@post = current_user.posts.find(params[:id])
+	end
+
+	def update
+		@group = Group.find(params[:group_id])
+		@post = current_user.posts.find(params[:id])
+
+		if @post.update(post_params)
+			redirect_to group_path(@group), notice: "修改文章成功"
+		else 
+			render :edit
+		end
+	end
+
+	def destroy			
+		@group = Group.find(params[:group_id])
+		@post = current_user.posts.find(params[:id])
+		
+		@post.destroy
+		redirect_to group_path(@group), alert: "文章已刪除"
+	end
+
 	private
 	def post_params
 		params.require(:post).permit(:content)
